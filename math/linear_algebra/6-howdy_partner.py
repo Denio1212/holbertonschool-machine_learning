32925eaa0335eef31003143c85c8d33b91581139
Denio1212 <shehuden22@yahoo.com>
2024-01-24 16:33:10 +0000

    2 arrays together forever

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Returns a new list that contains both arrays concatenated together
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Returns a new list that contains both arrays concatenated
+    Args:
+        arr1: First array
+        arr2: Second array
+
+    Returns: A new list with both arrays concatenated
+    """
+    concatenated = arr1 + arr2
+    return concatenated
