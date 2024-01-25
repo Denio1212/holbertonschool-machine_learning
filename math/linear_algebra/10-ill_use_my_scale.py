458a13bdafca9cab41245eb88f34db5fd55ef5f8
Denio1212 <shehuden22@yahoo.com>
2024-01-25 13:14:47 +0000

    Shaping a scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Returns the shape of a given matrix
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy array
+    Args:
+        matrix: The matrix given
+
+    Returns: the shape as a tuple
+    """
+    return matrix.shape
