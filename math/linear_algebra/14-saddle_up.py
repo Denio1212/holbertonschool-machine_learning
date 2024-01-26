a144bb80db2e819b65c25e234b57a2d37c3f6bdf
Denio1212 <shehuden22@yahoo.com>
2024-01-26 15:53:21 +0000

    Took 30 secs

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Performs matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Ain't having numpy arrays nice
+    Args:
+        mat1: first matrix
+        mat2: second matrix
+
+    Returns: result of matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
