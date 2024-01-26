1602e0e0cf9ba684de1c29490d89ea94731eaa8c
Denio1212 <shehuden22@yahoo.com>
2024-01-26 15:24:06 +0000

    Concat across axis

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -22,7 +22,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
         concat_2d_mat = mat1 + mat2
     elif axis == 1 and len(mat1) == len(mat2):
-        concat_2d_mat = [[row1 + row2] for row1, row2 in zip(mat1, mat2)]
+        concat_2d_mat = [row1 + row2 for row1, row2 in zip(mat1, mat2)]
     else:
         return None
     return concat_2d_mat
