4de0d7e1b19a5f6b2e06c9e082379c98c1520cfb
Deni1212 <shehuden22@yahoo.com>
2024-01-30 14:37:01 +0000

    squares i and then gives the sigma

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python3
-"""
-Calculates the sigma notation of a given n
-"""
-
-
-def summation_i_squared(n):
+ummation_i_squared(n):
     """
-    Summarizes the sigma of given n
+    Summarizes the sigma of given n without loops
     Args:
         n: The Iterator
 
     Returns: The sum
     """
+    if n is not type(int):
+        return None
     return int(n * (n + 1) * (2 * n + 1) / 6)
