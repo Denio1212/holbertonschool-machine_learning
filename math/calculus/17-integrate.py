5fcdc78c40dfca101f133158f312500daaffab59
Deni1212 <shehuden22@yahoo.com>
2024-02-07 12:33:58 +0000

    Big boi

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 """
 calculates the integral of a polynomial without importing
 """
@@ -14,7 +14,7 @@ def poly_integral(poly, C=0):
         return None
     integral_result = [0]
     for power, coef in enumerate(poly):
-        if not isinstance(coef, float) or not isinstance(coef, int):
+        if not isinstance(coef, (float, int)):
             return None
         integral_result.append(coef / (power + 1))
         integral_result = [int(coef) if coef.is_integer()
