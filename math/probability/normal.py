a639a7cdb8466506955a9703a62ba0aeed096364
Denio <shehuden22@yahoo.com>
2024-02-11 17:08:56 +0100

    task 6

---
+++ math/probability/normal.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Creates a class that represents normal distribution
+"""
+
+class Normal:
+    """
+    Houses a constructor with a mean and a standard deviation
+    """
+    def __init__(self, data=None, mean=0, stddev=1.):
+        """
+        :param data: the list of data used
+        :param mean: mean of the deviation
+        :param stddev: standard deviation of the distribution
+        sets the instance attributes mean and stddev as floats
+
+        if data is not given, use the given mean and standard deviation
+
+        if stddev is not a positive value or is 0 raise value error:
+        "stddev must be a positive value"
+
+        if data is given, calculate the mean and standard deviation of data
+        if data is not a list, raise type error:
+        "data must be a list"
+
+        if data is < 2, raise value error:
+        "data must contain multiple values"
+        """
+        self.mean = float(mean)
+        self.stddev = float(stddev)
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = (sum((ent - self.mean) ** 2 for ent in data) / len(data)) ** 0.5
