acd0da5b8e3bdf9f006a6504a463efef7859cd52
Denio <shehuden22@yahoo.com>
2024-02-20 16:09:46 +0100

    task 2

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -16,7 +16,7 @@ def marginal(x, n, P, Pr):
     :return: marginal probability of obtaining x and n
     Im too lazy to write the conditions so yea!
     """
-    if n <= 0 or not isinstance(n, int):
+    if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
 
     if not isinstance(x, int) or x < 0:
@@ -30,7 +30,7 @@ def marginal(x, n, P, Pr):
         raise TypeError("P must be a 1D numpy.ndarray")
 
     if not isinstance(Pr, np.ndarray) or Pr.ndim != 1 or Pr.shape != P.shape:
-        raise TypeError("Pr must be a 1D numpy.ndarray with the same"
+        raise TypeError("Pr must be a numpy.ndarray with the same"
                         " shape as P")
 
     if np.any(P < 0) or np.any(P > 1):
