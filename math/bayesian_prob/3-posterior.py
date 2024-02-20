8d3fa8f7301e8b45c3ae7d3d8d23c035983e3924
Denio <shehuden22@yahoo.com>
2024-02-20 16:21:59 +0100

    task 3

--- math/bayesian_prob/3-posterior.py
+++ math/bayesian_prob/3-posterior.py
@@ -31,6 +31,15 @@ def posterior(x, n, P, Pr):
         raise TypeError("Pr must be a numpy.ndarray with the same"
                         " shape as P")
 
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
     n_fact = np.math.factorial(n)
     x_fact = np.math.factorial(x)
     n_x_fact = np.math.factorial(n - x)
