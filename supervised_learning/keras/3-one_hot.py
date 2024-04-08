e7c5c20e66f67b4a4a9784d705dcc44ad6d7c50a
Denio <shehuden22@yahoo.com>
2024-04-08 16:34:01 +0200

    Task 3, one hot encode

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Converts a vector of integers into a one-hot matrix
+"""
+
+
+import tensorflow.keras as keras
+
+
+def one_hot(labels, classes=None):
+    """
+    Converts a matrix of integers into a one-hot matrix
+    """
+    keras.utils.to_categorical(labels, num_classes=classes)
