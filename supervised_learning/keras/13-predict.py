c66a7a90fc2a67b569037741f58062ea8db78b50
Denio <shehuden22@yahoo.com>
2024-04-12 11:50:27 +0200

    task 12, predicts test data

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Make prediction using neural network
+"""
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """
+        makes a prediction using a neural network
+
+        :param network: NN to make prediction with
+        :param data: input data
+        :param verbose: boolean, output printed or not
+
+        :return: prediction of data
+    """
+    return network.predict(data, verbose=verbose)
