78ce9dba888b2976d51c8f1fa6857819c036dd58
Denio <shehuden22@yahoo.com>
2024-04-15 16:35:33 +0200

    Task 6, Momentum based gradient descent with tensorflow 2

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Updates a variable using a gradient descent algorithm with momentum
+optimization.
+"""
+
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    Creates the training operation for a neural network in tensorflow
+
+    :param loss: loss of the network
+
+    :param alpha: learning rate
+
+    :param beta1: momentum weight
+    """
+    optimizer = tf.train.MomentumOptimizer(learning_rate=alpha, momentum=beta1)
+    train_op = optimizer.minimize(loss)
+    return train_op
