174eac6844e242a5307e595f3970535cca7164ca
Denio <shehuden22@yahoo.com>
2024-04-26 15:59:30 +0200

    Task 4, F-1 Score (2 * (precision * sensitivity)) / precision * sensitivity

---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Calculates the f1 score// 2 * (precision * recall / (precision + recall)) //
+"""
+
+
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """
+    Calculates the f1 score// 2 * (precision * recall / (precision + recall))
+
+    :param confusion: confusion matrix of shape (classes, classes)
+    Classes1 has true labels
+    Classes2 has predicted labels
+
+    :return: f1 score // 2 * (precision * recall / (precision + recall))
+    """
+    num_classes = confusion.shape[0]
+    f_1 = np.zeros((num_classes,))
+
+    precisions = precision(confusion)
+    sensitivites = sensitivity(confusion)
+
+    for i in range(num_classes):
+        f_1[i] = ((2 * (precisions[i] * sensitivites[i]))
+               / (precisions[i] + sensitivites[i]))
+
+    return f_1
