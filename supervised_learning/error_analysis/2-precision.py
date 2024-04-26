b29139c677590b54362343ae9cbbadae68dbb2c3
Denio <shehuden22@yahoo.com>
2024-04-26 15:23:50 +0200

    Task 1, Precision (TP / (TP + FP))

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Calculates the precision of each class.
+"""
+
+
+import numpy as np
+
+
+def precision(confusion):
+    """
+    Calculates the precision of each class.
+
+    :param confusion: confusion matrix shape (classes, classes)
+    Where classes1 is number of classes with correct predictions.
+    And classes2 is number of classes with incorrect predictions.
+
+    :return: precision
+    """
+    num_classes = confusion.shape[0]
+    precision = np.zeros((num_classes,))
+
+    for i in range(num_classes):
+            t_positive = confusion[i, i]
+            f_positive = np.sum(confusion[:, i]) - t_positive
+
+            precision[i] += t_positive / (t_positive + f_positive)
+
+    return precision
