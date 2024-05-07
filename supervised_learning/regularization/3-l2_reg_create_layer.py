329e78277c00d6abbee2fc40b2c87578e180dcbf
Denio <shehuden22@yahoo.com>
2024-05-07 13:23:36 +0200

    Task 3, Layer Creation w regularization

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python3
 """
-Creates a layer in tensorflow 1 that includes a
-l2 regularization parameter.
+    Create layer with L2 regularization
 """
 
 import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    Creates a layer in tensorflow 1 that includes l2 regularization
-
-    :param prev: previous layer
-
-    :param n: layer size
-
-    :param activation: activation function
-
-    :param lambtha: regularization parameter
-
-    :return: new layer output
+        Function that creates a tensorflow layer includes L2 regularization
     """
-    regualizer = tf.keras.regularizers.l2(lambtha)
-    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
+    # set initialization to He et. al
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                        mode='fan_avg')
 
-    l2_layer = tf.layers.dense(n, activation=activation,
-                               kernel_initializer=init,
-                               kernel_regularizer=regualizer,
-                               name="l2_layer")
-    output = l2_layer(prev)
+    # create layer Dense with parameters
+    new_layer = (
+        tf.layers.Dense(n,
+                        activation=activation,
+                        kernel_initializer=initializer,
+                        kernel_regularizer=tf.keras.regularizers.l2(lambtha),
+                        name="layer"))
+
+    # apply layer to input
+    output = new_layer(prev)
 
     return output
---
+++ supervised_learning/regularization/personal_file/3-l2_reg_create_layer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Creates a layer in tensorflow 1 that includes a
+l2 regularization parameter.
+"""
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Creates a layer in tensorflow 1 that includes l2 regularization
+
+    :param prev: previous layer
+
+    :param n: layer size
+
+    :param activation: activation function
+
+    :param lambtha: regularization parameter
+
+    :return: new layer output
+    """
+    regularizer = tf.keras.regularizers.l2(lambtha)
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
+
+    l2_layer = tf.layers.dense(n, activation=activation,
+                               kernel_initializer=init,
+                               kernel_regularizer=regularizer,
+                               name="l2_layer")
+    output = l2_layer(prev)
+
+    return output
