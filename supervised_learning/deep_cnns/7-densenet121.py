62a7ceed035e0a35f4e71dd02812ed48c07129f3
Denio <shehuden22@yahoo.com>
2024-06-10 16:31:09 +0200

    Task 7, DenseNet architecture

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -5,7 +5,7 @@ using Keras model
 """
 
 
-import tensorflow.keras as K
+from tensorflow import keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
 
