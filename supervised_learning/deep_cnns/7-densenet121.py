3c5abab6255d2e2f7a7ebc58a98788e61b2f06b2
Denio <shehuden22@yahoo.com>
2024-06-10 16:33:12 +0200

    Task 7, DenseNet architecture

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -34,14 +34,17 @@ def densenet121(growth_rate=32, compression=1.0):
     img_input = K.Input(shape=(224, 224, 3))
     Batch_NormC0 = K.layers.BatchNormalization(axis=3)(img_input)
     ReLUC0 = K.layers.Activation(activation)(Batch_NormC0)
+
     C0 = K.layers.Conv2D(filters=64,
                          kernel_size=(7, 7),
                          padding='same',
                          strides=(2, 2),
                          kernel_initializer=init)(ReLUC0)
+
     MP1 = K.layers.MaxPooling2D(pool_size=(3, 3),
                                 strides=(2, 2),
                                 padding='same')(C0)
+
     DB2, nb_filters = dense_block(MP1, 64, growth_rate, 6)
     TL3, nb_filters = transition_layer(DB2, nb_filters, compression)
     DB4, nb_filters = dense_block(TL3, nb_filters, growth_rate, 12)
@@ -53,6 +56,7 @@ def densenet121(growth_rate=32, compression=1.0):
     AP9 = K.layers.AveragePooling2D(pool_size=(7, 7),
                                     strides=(1, 1),
                                     padding='valid')(DB8)
+
     output = K.layers.Dense(1000,
                             activation='softmax',
                             kernel_initializer=init)(AP9)
