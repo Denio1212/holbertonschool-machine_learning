6a5441c8e5fbe979257440d625b5a8c629e57d0e
Denio <113891603+Denio1212@users.noreply.github.com>
2024-08-05 17:23:24 +0200

    Task 3, Switch from gensim to keras

---
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Converts a gensim word2vec model to a keras model
+"""
+
+
+def gensim_to_keras(model):
+    """
+    Converts a gensim word2vec model to a keras model
+
+    Parameters:
+        model: gensim word2vec model which is trained
+    Returns:
+        Keras Embedding layer
+    """
+    return model.wv.get_keras_embedding(True)
+
+
