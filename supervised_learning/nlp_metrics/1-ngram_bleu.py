16beb4cf43459149ca78970f9e43982b39b6dcb9
Denio <113891603+Denio1212@users.noreply.github.com>
2024-08-21 14:38:43 +0200

    Task 1, n-gram BLEU

--- supervised_learning/nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/nlp_metrics/1-ngram_bleu.py
@@ -1,43 +1,33 @@
 #!/usr/bin/env python3
-"""
-Calculates the n-gram BLEU score for a sentence
-"""
-
-
+"""Cumulative N-gram BLEU Score"""
 import numpy as np
 
 
-def ngram_bleu(references, sentence, n):
+def cumulative_bleu(references, sentence, n):
     """
-    Calculates the n-gram BLEU score for a sentence
+    Calculates the cumulative n-gram BLEU score for a sentence:
+        - references is a list of reference translations
+        - each reference translation is a list of the words in the translation
+        - sentence is a list containing the model proposed sentence
+        - n is the size of the n-gram to use for evaluation
+        - All n-gram scores should be weighted evenly
 
-    Parameters:
-        references (list): list of reference translations
-            each reference translation is a list of the words in
-            the translation
-
-        sentence (list): list of words in the model proposed sentence
-
-        n: size of the n-gram to be used
-
-    Returns:
-        n-gram BLEU score
-
-    Bp is belief propagation
+    Returns: the cumulative n-gram BLEU score
     """
+
     BP = min(1, np.exp(1 - len(min(references, key=len)) / len(sentence)))
-    n_grams = []
-    n_grams_ref = 0
+    precision = []
 
-    for reference in references:
-        n_grams_ref = []
-        for i in range(len(sentence) - (n - 1)):
-            if any(sentence[i:i + n] == reference[j:j + n]
-                   for j in range(len(reference) - (n - 1))) and \
-                    sentence[i:i + n] not in n_grams_ref:
-                n_grams_ref.append(sentence[i:i + n])
-        n_grams.append(len(n_grams_ref))
+    for m in range(1, n+1):
+        n_grams = []
+        for reference in references:
+            n_grams_ref = []
+            for i in range(len(sentence) - (m - 1)):
+                if any(sentence[i:i + m] == reference[j:j+m]
+                       for j in range(len(reference) - (m - 1))) and \
+                        sentence[i:i+m] not in n_grams_ref:
+                    n_grams_ref.append(sentence[i:i+m])
+            n_grams.append(len(n_grams_ref))
+        precision.append(max(n_grams) / (i + 1))
 
-    precision = max(n_grams) / (i + 1)
-
-    return BP * np.exp(np.log(precision))
+    return BP * np.exp(np.mean(np.log(precision)))
