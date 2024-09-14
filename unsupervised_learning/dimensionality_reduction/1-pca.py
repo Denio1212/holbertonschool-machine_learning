69e98c6f52b8e1953f8e2611d4feb2a6908219e3
Denio <113891603+Denio1212@users.noreply.github.com>
2024-09-14 10:37:01 +0200

    Dimensionality Reduction, Task 1

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,19 @@
+# %%
+# !/usr/bin/env python3
+"""
+Performs PCA on a given dataset
+"""
+
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Performs PCA on a given dataset
+    """
+    mean = np.mean(X, axis=0, keepdims=True)
+    A = X - mean
+    u, s, v = np.linalg.svd(A)
+    W = v.T[:, :ndim]
+    T = np.matmul(A, W)
+    return T
