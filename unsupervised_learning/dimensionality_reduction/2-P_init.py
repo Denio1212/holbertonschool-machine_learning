4d47d5ddbfe2ec8d208d26afc4aab7ccec0cbb50
Denio <113891603+Denio1212@users.noreply.github.com>
2024-09-15 11:14:15 +0200

    T-SNE, task 1

---
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Initializes all variables required for to calculate the P affinities in T-sne
+"""
+
+import numpy as np
+
+
+def P_init(X, perplexity):
+    """
+    Initializes all variables required for to calculate the P affinities in T-sne
+
+    Parameters:
+        X is a numpy.ndarray of shape (n, d) containing the dataset
+        to be transformed by t-SNE
+            -> n is the number of data points
+            -> d is the number of dimensions in each point
+
+        perplexity is the perplexity that all Gaussian distributions have
+
+    Returns:
+        (D, P, betas, H)
+            -> D: a numpy.ndarray of shape (n, n) that calculates
+            the squared pairwise distance between two data points
+                * The diagonal of D should be 0s
+
+            -> P: a numpy.ndarray of shape (n, n) initialized to all 0‘s that
+            will contain the P affinities
+
+            -> betas: a numpy.ndarray of shape (n, 1) initialized to all 1’s
+            that will contain all of the beta values
+
+
+            -> H is the Shannon entropy for perplexity perplexity with a base of 2
+    """
+    n, d = X.shape
+    X_sum = np.sum(np.square(X), axis=1)
+    D = np.add(np.add(-2 * np.dot(X, X.T), X_sum).T, X_sum)
+    np.fill_diagonal(D, 0)
+
+    P = np.zeros((n, n))
+
+    betas = np.ones((n, 1))
+
+    H = np.log2(perplexity)
+
+    return D, P, betas, H
