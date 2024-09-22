f47f61a90d35b42fb254b8e47c9956233c3da213
Denio <113891603+Denio1212@users.noreply.github.com>
2024-09-22 18:48:28 +0200

    Clustering, task 5, PDF function

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Calculates the probability density function of a Gaussian Distribution
+PDF function
 """
 
 import numpy as np
@@ -8,13 +8,16 @@ import numpy as np
 
 def pdf(X, m, S):
     """
-    Calculates the probability density function of a Gaussian Distribution
+    Probability Density Function of gaussian distributions
     """
+
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
     if not isinstance(m, np.ndarray) or len(m.shape) != 1:
         return None
     if not isinstance(S, np.ndarray) or len(S.shape) != 2:
+        return None
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
         return None
     if S.shape[0] != S.shape[1]:
         return None
@@ -27,13 +30,13 @@ def pdf(X, m, S):
 
     inv_S = np.linalg.inv(S)
 
-    part_1_dem = np.sqrt(det_S) * ((2 * np.pi) ** (d / 2))
+    part_1_dem = np.sqrt(det_S) * ((2 * np.pi) ** (d/2))
 
     part_2 = np.matmul(x_m, inv_S)
 
     part_2_1 = np.sum(x_m * part_2, axis=1)
 
-    part_2_2 = np.sum(x_m * part_2, axis=1)
+    part_2_2 = np.exp(part_2_1 / -2)
 
     pdf = part_2_2 / part_1_dem
     P = np.where(pdf < 1e-300, 1e-300, pdf)
